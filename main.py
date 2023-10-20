67478fd44f54190923397cb194bd66d8ad68f624
ikemura-ren <utip.saikai.nozomu@icloud.com>
2023-10-20 14:17:26 +0900

    main.pyの作成

---
+++ main.py
@@ -0,0 +1,25 @@
+def main():
+    while True:
+        print("選択してください：")
+        print("1: 選択肢1")
+        print("2: 選択肢2")
+        print("3: 選択肢3")
+        print("q: 終了")
+
+        choice = input("> ")
+
+        if choice == "1":
+            print("選択肢1が選ばれました。")
+        elif choice == "2":
+            print("選択肢2が選ばれました。")
+        elif choice == "3":
+            print("選択肢3が選ばれました。")
+        elif choice == "q":
+            print("プログラムを終了します。")
+            break
+        else:
+            print("無効な入力です。もう一度選択してください。")
+
+if __name__ == "__main__":
+    main()
+
